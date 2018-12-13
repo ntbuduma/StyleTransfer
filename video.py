fbc343952939faf3831487ab23da7e1bd0fda133
Seri Choi <serichoi@mit.edu>
2018-12-12 19:11:18 -0500

    final push

--- video.py
+++ video.py
@@ -1,10 +1,10 @@
 import cv2
 
-vidcap = cv2.VideoCapture("videos/SampleVideo_1280x720_1mb.mp4")
+vidcap = cv2.VideoCapture("results/video_test_german_transition_logistic_seri.mp4")
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
+  cv2.imwrite("logistic/l%d.jpg" % count, image)     # save frame as JPEG file      
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
